b88a9840786e713a8c5b1d9c4855a5b20c4643e6
Doğukan Matuloğlu <dogukan.matuloglu@fimeksus.com>
2023-01-25 14:39:59 +0300

    Entityler Oluşturuldu

--- ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -14,9 +14,9 @@ namespace ProgrammersBlog.Shared.Entities.Abstract
         public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
         public virtual bool IsDeleted { get; set; } = false;
         public virtual bool IsActive { get; set; } = true;
-        public virtual string CreatedByName { get; set; }
-        public virtual string ModifieddByName { get; set; }
-
+        public virtual string CreatedByName { get; set; } = "Admin";
+        public virtual string ModifieddByName { get; set; } = "Admin";
+        public virtual string Note { get; set; }
 
     }
 }
