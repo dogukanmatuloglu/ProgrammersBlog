d0c8d991412fa68cea816fc74bd97f76e4cacae9
Dogukan Matuloglu <dogukan.matul@gmail.com>
2023-01-28 13:40:08 +0300

    UnitOfWord Design Pattern uygulamaya implemente edildi

--- ProgrammersBlog.Data/Abstract/IUserRepository.cs
+++ ProgrammersBlog.Data/Abstract/IUserRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Data.Abstract
 {
-    internal interface IUserRepository:IEntityRepository<User>
+    public interface IUserRepository:IEntityRepository<User>
     {
     }
 }
