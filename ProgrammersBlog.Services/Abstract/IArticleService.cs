d7d7fb2a183142fe483278738f32dbc7d8465f8b
Dogukan Matuloglu <dogukan.matul@gmail.com>
2023-01-28 23:06:48 +0300

    IArticleService Interface oluşturuldu

---
+++ ProgrammersBlog.Services/Abstract/IArticleService.cs
@@ -0,0 +1,25 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.Abstract
+{
+    public interface IArticleService
+    {
+
+        Task<IDataResult<ArticleDto>> Get(int id);
+        Task<IDataResult<ArticleListDto>> GetAll();
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeleted();
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeletedAndActive();
+        Task<IDataResult<ArticleListDto>> GetAllByCategory(int categoryId);
+        Task<IResult> Add(ArticleAddDto cArticleAddDto, string createdByName);
+        Task<IResult> Update(ArticleUpdateDto ArticleUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int articleId, string modifiedByName);
+        Task<IResult> HardDelete(int articleId);
+    }
+}
