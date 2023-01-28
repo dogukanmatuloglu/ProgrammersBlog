f82d18e2519e24c627bcc7eec2a9a83b04ac726e
Dogukan Matuloglu <dogukan.matul@gmail.com>
2023-01-28 23:34:53 +0300

    Article Dto sınıfları oluşturuldu

--- ProgrammersBlog.Entities/Dtos/ArticleListDto.cs
+++ ProgrammersBlog.Entities/Dtos/ArticleListDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Entities.Dtos
 {
-    public class ArticleListDto
+    public class ArticleListDto:DtoGetBase
     {
+        public IList<Article> Articles { get; set; }
+
     }
 }
