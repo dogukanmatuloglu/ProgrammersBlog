7093bebcc3bc65addfd88543155e2118f4b25e58
Dogukan Matuloglu <dogukan.matul@gmail.com>
2023-01-28 17:15:35 +0300

    ICategoryService Interface oluşturuldu

---
+++ ProgrammersBlog.Services/Abstract/ICategoryService.cs
@@ -0,0 +1,22 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<IDataResult<Category>> Get(int id);
+        Task<IDataResult<IList<Category>>> GetAll();
+        Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName);
+        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int categoryId);
+        Task<IResult> HardDelete(int categoryId);
+    }
+}
