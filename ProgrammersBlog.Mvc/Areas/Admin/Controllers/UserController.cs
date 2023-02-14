9803d175e54d0e959320e78b7eca267d8705bb05
Dogukan Matuloglu <dogukan.matul@gmail.com>
2023-02-14 20:36:43 +0300

    ImageUpload Metodu Oluşturuldu

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProgrammersBlog.Entities.Concrete;
 using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Extensions;
 using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 
 namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
@@ -11,10 +12,12 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<User> _userManager;
+        private readonly IWebHostEnvironment _env;
 
-        public UserController(UserManager<User> userManager)
+        public UserController(UserManager<User> userManager, IWebHostEnvironment env = null)
         {
             _userManager = userManager;
+            _env = env;
         }
 
         public async Task<IActionResult> Index()
@@ -31,6 +34,21 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
         public IActionResult Add()
         {
             return PartialView("_UserAddPartial");
+        }
+
+        public async Task<string> ImageUpload(UserAddDto userAddDto)
+        {
+            string wwwroot = _env.WebRootPath;
+            string fileExtension = Path.GetExtension(userAddDto.Picture.FileName);
+            DateTime dateTime = DateTime.Now;
+            string fileName=$"{userAddDto.UserName}_{dateTime.FullDateAndTimeStringWithUnderscore()}{fileExtension}";
+            var path=Path.Combine($"{wwwroot}/img",fileName);
+            await using(var stream=new FileStream(path, FileMode.Create))
+            {
+                await userAddDto.Picture.CopyToAsync(stream);
+            }
+
+            return fileName;
         }
     }
 }
