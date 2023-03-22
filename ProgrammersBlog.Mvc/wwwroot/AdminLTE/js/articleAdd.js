a865cdf6d4f94c75d056f7af7257884fb2c769d2
dogukan matuloğlu <dogukan.matul@gmail.com>
2023-03-22 22:10:48 +0300

    jQuery UI Kütüphanesi ve DatePicker Eklentisi Eklendi

--- ProgrammersBlog.Mvc/wwwroot/AdminLTE/js/articleAdd.js
+++ ProgrammersBlog.Mvc/wwwroot/AdminLTE/js/articleAdd.js
@@ -31,4 +31,34 @@ $(document).ready(function () {
         placeholder: "Lütfen Bir Kategori Seçiniz..",
         allowClear: true
     });
+
+    //Select2
+
+    //JQuery UI-DataPicker
+    $(function () {
+        $("#datepicker").datepicker({
+            closeText: "kapat",
+            prevText: "&#x3C;geri",
+            nextText: "ileri&#x3e",
+            currentText: "bugün",
+            monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+                "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+            monthNamesShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz",
+                "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
+            dayNames: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+            dayNamesShort: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            dayNamesMin: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            weekHeader: "Hf",
+            dateFormat: "dd.mm.yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "",
+            duration: 1000,
+            minDate: -3,
+            maxDate:3
+
+        });
+    });
+     //JQuery UI-DataPicker
 });
