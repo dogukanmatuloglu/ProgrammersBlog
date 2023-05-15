49b5ba8077a5c28605391a9680c4882bb5bee612
dogukan matuloğlu <dogukan.matul@gmail.com>
2023-05-15 20:15:22 +0300

    ArticleController Detail Action Oluşturuldu

---
+++ ProgrammersBlog.Mvc/Controllers/ArticleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Services.Abstract;
+
+namespace ProgrammersBlog.Mvc.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Detail(int articleId)
+        {
+            var articleResult=await _articleService.GetAsync(articleId);
+            if (articleResult.ResultStatus==Shared.Utilities.Results.ComplexTypes.ResultStatus.Success)
+            {
+                return View(articleResult.Data);
+            }
+            return NotFound();
+        }
+    }
+}
