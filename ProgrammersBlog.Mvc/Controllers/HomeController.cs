11a823b717ba63eb8085ca4ade6e971b3948b0b3
dogukan matuloğlu <dogukan.matul@gmail.com>
2023-05-26 20:50:48 +0300

    Anasayfa için (Sorting) Fonksiyonelliği  Eklendi.

--- ProgrammersBlog.Mvc/Controllers/HomeController.cs
+++ ProgrammersBlog.Mvc/Controllers/HomeController.cs
@@ -12,9 +12,9 @@ namespace ProgrammersBlog.Mvc.Controllers
             _articleService = articleService;
         }
 
-        public async Task<IActionResult> Index(int? categoryId,int currentPage=1,int pageSize=5)
+        public async Task<IActionResult> Index(int? categoryId,int currentPage=1,int pageSize=5,bool isAscending=false)
         {
-            var articlesResult = categoryId == null ? await _articleService.GetAllByPagingAsync(null, currentPage, pageSize) : await _articleService.GetAllByPagingAsync(categoryId.Value, currentPage, pageSize);
+            var articlesResult = categoryId == null ? await _articleService.GetAllByPagingAsync(null, currentPage, pageSize,isAscending) : await _articleService.GetAllByPagingAsync(categoryId.Value, currentPage, pageSize,isAscending);
             return View(articlesResult.Data);
            
 
